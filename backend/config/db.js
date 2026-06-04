cec7b04c12881927568ef4921f11ce6ca1b1c177
gaurav-kasaudhan786 <gskasaudhan456@gmail.com>
2026-06-04 13:38:49 +0530

    Clean up db.js by removing commented code
    Removed commented-out code for database connection.

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,5 @@
 import mongoose from "mongoose";
 
-// export const connectDB = async () => {
-//   await mongoose
-//     .connect(
-//       "mongodb+srv://aayush_db_user:tasktracker328@cluster0.eemtdih.mongodb.net/TaskTacker"
-//     )
-//     .then(() => {
-//       console.log("DB connected");
-//     });
-// };
-
 export const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
